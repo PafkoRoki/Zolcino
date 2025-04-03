a74cf7e52c4d9e73c6ce29bd4ba6efe60c7d0bad
Pafko <pafko.roki@gmail.com>
2025-04-03 18:27:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,20 +29,30 @@ window.onload = function () {
   
 	// Function to open the modal and update content
 	function openModal(target) {
-	  const regionName = target.dataset.name || "Nieznany region";
-	  const regionFunction = target.getAttribute("region-function") || "Brak informacji o funkcji regionu";
-	  const area = target.getAttribute("data-area") || "Brak informacji o powierzchni";
-	  const building = target.getAttribute("data-building") || "Brak informacji o budynku";
-  
-	  modal.classList.add("active");
-	  modalContent.innerHTML = `
-		<h1>${regionName}</h1>
-		<p>Funkcja regionu: <b>${regionFunction}</b></p>
-		<p>Powierzchnia: <b>${area}</b></p>
-		<p>Budynek: <b>${building}</b></p>
-	  `;
-	}
-  
+		const regionName = target.dataset.name || "Nieznany region";
+		const regionFunction = target.getAttribute("region-function") || "Brak informacji o funkcji regionu";
+		const area = target.getAttribute("data-area") || "Brak informacji o powierzchni";
+		const building = target.getAttribute("data-building") || "Brak informacji o budynku";
+		const quarter = target.getAttribute("data-quarter") || "Brak informacji o kwartale";
+		const pum = target.getAttribute("data-pum") || "Brak informacji o PUM";
+		const pumk = target.getAttribute("data-pumk") || "Brak informacji o cenie PUM";
+		const price = target.getAttribute("data-price") || "Brak informacji o cenie";
+	  
+		modal.classList.add("active");
+		modalContent.innerHTML = `
+		  <h1>${regionName}</h1>
+		  <hr> <!-- Horizontal line below the region name -->
+		  <table class="modal-table">
+			<tr><td>Funkcja regionu:</td><td><b>${regionFunction}</b></td></tr>
+			<tr><td>Powierzchnia:</td><td><b>${area}</b></td></tr>
+			<tr><td>Budynek:</td><td><b>${building}</b></td></tr>
+			<tr><td>Kwartał:</td><td><b>${quarter}</b></td></tr>
+			<tr><td>Przybliżony PUM:</td><td><b>${pum}</b></td></tr>
+			<tr><td>Cena 1 m² PUM:</td><td><b>${pumk}</b></td></tr>
+			<tr><td>Cena:</td><td><b>${price}</b></td></tr>
+		  </table>
+		`;
+	  }
 	// Close modal when the close button is clicked
 	closeModal.addEventListener("click", () => {
 	  modal.classList.remove("active");
